7cadb02b264e7d8ade26359b982fb73c4a7f0f59
Josue Venegas Almonacid <josuevenegasalmonacid@gmail.com>
2021-12-14 17:41:58 -0300

    Servidores fulcrum guardan registro

--- lab3/lab3.go
+++ lab3/lab3.go
@@ -1,46 +1,47 @@
 package lab3
 
 import (
-	"log"
-	"golang.org/x/net/context"
-	"math/rand"
-	"time"
-	"os"
-	"strings"
 	"bufio"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
 	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/net/context"
 )
 
 type Server struct {
-	process bool
-	planeta string
-	ciudad string
+	process  bool
+	planeta  string
+	ciudad   string
 	soldados int32
 	intvalue int32
-    servidor int32
-	merge bool
-	Valor int32  
-	X int32
-	Y int32
-	Z int32
+	servidor int32
+	merge    bool
+	Valor    int32
+	X        int32
+	Y        int32
+	Z        int32
 }
 
-type reloj struct{   ///////////////////////////
-	x int
-	y int
-	z int
+type reloj struct { ///////////////////////////
+	x        int
+	y        int
+	z        int
 	servidor int
 }
 
-type rj struct{   ///////////////////////////
+type rj struct { ///////////////////////////
 	x int32
 	y int32
 	z int32
 }
 
-var mapaDos = make(map[string] reloj)
+var mapaDos = make(map[string]reloj)
 
 func ReadPlanet(planeta string) {
 	fileName := planeta + ".txt"
@@ -65,7 +66,7 @@ func ReadPlanet(planeta string) {
 	}
 }
 
-func AddCity(planeta string, ciudad string, soldados string) {
+func AddCity(planeta string, ciudad string, soldados string) bool {
 	fileName := planeta + ".txt"
 	var existe = false
 
@@ -91,7 +92,7 @@ func AddCity(planeta string, ciudad string, soldados string) {
 		//Si existe la ciudad, no se puede agregar
 		if existe {
 			fmt.Printf("Ciudad %s ya existe\n", ciudad)
-			return
+			return false
 		}
 	}
 
@@ -106,9 +107,10 @@ func AddCity(planeta string, ciudad string, soldados string) {
 	//Agregamos la ciudad
 	file.WriteString(planeta + " " + ciudad + " " + soldados + "\n")
 	fmt.Printf("Registro [%s, %s, %s] creado\n", planeta, ciudad, soldados)
+	return true
 }
 
-func ChangeInfo(tipo string, planeta string, ciudad string, dato_new string) {
+func ChangeInfo(tipo string, planeta string, ciudad string, dato_new string) bool {
 	oldFileName := planeta + ".txt"
 	tempFileName := planeta + "_temp.txt"
 
@@ -174,23 +176,27 @@ func ChangeInfo(tipo string, planeta string, ciudad string, dato_new string) {
 
 		if !existe {
 			fmt.Printf("Ciudad %s no existe\n", ciudad)
+			return false
+		} else {
+			return true
 		}
 
 	} else {
 		fmt.Printf("Planeta %s no existe\n", oldFileName)
+		return true
 	}
 }
 
-func DeleteCity(planeta string, ciudad string) {
-	ChangeInfo("DeleteCity", planeta, ciudad, "")
+func DeleteCity(planeta string, ciudad string) bool {
+	return ChangeInfo("DeleteCity", planeta, ciudad, "")
 }
 
-func UpdateName(planeta string, ciudad_old string, ciudad_new string) {
-	ChangeInfo("UpdateName", planeta, ciudad_old, ciudad_new)
+func UpdateName(planeta string, ciudad_old string, ciudad_new string) bool {
+	return ChangeInfo("UpdateName", planeta, ciudad_old, ciudad_new)
 }
 
-func UpdateNumber(planeta string, ciudad string, soldados_new string) {
-	ChangeInfo("UpdateNumber", planeta, ciudad, soldados_new)
+func UpdateNumber(planeta string, ciudad string, soldados_new string) bool {
+	return ChangeInfo("UpdateNumber", planeta, ciudad, soldados_new)
 }
 
 func AddRegistro(servidor string, operacion string) {
@@ -265,33 +271,36 @@ func UpdatePlanet(planeta string, contenido string) {
 	fmt.Printf("Planeta %s mergeado\n", planeta)
 }
 
-func (s *Server) Enviarinfo(ctx context.Context, in *Info) (*Info, error){
-	return &Info{Planeta: "Tatooine",Ciudad: "Mos_Eisley", Soldados: 5},nil
+func (s *Server) Enviarinfo(ctx context.Context, in *Info) (*Info, error) {
+	return &Info{Planeta: "Tatooine", Ciudad: "Mos_Eisley", Soldados: 5}, nil
 }
 
-func (s *Server) Fulcrum(ctx context.Context, in *Operacion) (*Reloj, error){
-	log.Printf("%s %s %s", in.Accion, in.Planeta,in.Ciudad)
+func (s *Server) Fulcrum(ctx context.Context, in *Operacion) (*Reloj, error) {
+	log.Printf("%s %s %s", in.Accion, in.Planeta, in.Ciudad)
 	var p bool
+	var soldados string
+	var op bool
 	if in.Accion == "AddCity" {
 		if in.Intvalue != -1 {
 			fmt.Println(in.Intvalue)
-			AddCity(in.Planeta, in.Ciudad, strconv.Itoa(int(in.Intvalue)))
-		}else{
-			AddCity(in.Planeta, in.Ciudad, "")
+			soldados = strconv.Itoa(int(in.Intvalue))
+		} else {
+			soldados = ""
 		}
+		op = AddCity(in.Planeta, in.Ciudad, soldados)
 		count, ok := mapaDos[in.Planeta]
 		if ok == false {
 			fmt.Println("el elemento no estaba", count)
 			var r1 reloj
-			if in.Servidor == 1{
+			if in.Servidor == 1 {
 				r1.x = 1
 				r1.y = 0
 				r1.z = 0
-			}else if in.Servidor == 2{
+			} else if in.Servidor == 2 {
 				r1.x = 0
 				r1.y = 1
 				r1.z = 0
-			}else{
+			} else {
 				r1.x = 0
 				r1.y = 0
 				r1.z = 1
@@ -301,116 +310,124 @@ func (s *Server) Fulcrum(ctx context.Context, in *Operacion) (*Reloj, error){
 			fmt.Println(mapaDos)
 		} else {
 			var r1 reloj
-			r1 = mapaDos[in.Planeta] 
-			if in.Servidor == 1{
+			r1 = mapaDos[in.Planeta]
+			if in.Servidor == 1 {
 				r1.x = r1.x + 1
-			}else if in.Servidor == 2{
+			} else if in.Servidor == 2 {
 				r1.y = r1.y + 1
-			}else{
+			} else {
 				r1.z = r1.z + 1
 			}
 			r1.servidor = int(in.Servidor)
 			mapaDos[in.Planeta] = r1
-		} 
-	}else if in.Accion == "DeleteCity"{
-		DeleteCity(in.Planeta, in.Ciudad)
+		}
+	} else if in.Accion == "DeleteCity" {
+		op = DeleteCity(in.Planeta, in.Ciudad)
 		delete(mapaDos, in.Planeta)
-	}else if in.Accion == "UpdateName"{
-		UpdateName(in.Planeta, in.Ciudad, in.Svalue)
+	} else if in.Accion == "UpdateName" {
+		op = UpdateName(in.Planeta, in.Ciudad, in.Svalue)
 		count, ok := mapaDos[in.Planeta]
 		if ok == true {
 			var r1 reloj
 			r1 = mapaDos[in.Planeta]
-			if in.Servidor == 1{
+			if in.Servidor == 1 {
 				r1.x = r1.x + 1
-			}else if in.Servidor == 2{
+			} else if in.Servidor == 2 {
 				r1.y = r1.y + 1
-			}else{
+			} else {
 				r1.z = r1.z + 1
 			}
 			r1.servidor = int(in.Servidor)
 			mapaDos[in.Planeta] = r1
-		}else{
+		} else {
 			fmt.Println("el elemento no estaba", count)
 			if s.merge == true {
+				ClearRegistro(strconv.Itoa(int(in.Servidor)))
 				p = true
 				s.merge = false
-			}else{
+			} else {
 				p = false
 			}
-			return &Reloj{Planeta: "",X:int32(0),Y:int32(0),Z:int32(0),Merge:p},nil
+			return &Reloj{Planeta: "", X: int32(0), Y: int32(0), Z: int32(0), Merge: p}, nil
 		}
-	}else if in.Accion == "UpdateNumber"{
-		UpdateNumber(in.Planeta, in.Ciudad, strconv.Itoa(int(in.Intvalue)))
+	} else if in.Accion == "UpdateNumber" {
+		op = UpdateNumber(in.Planeta, in.Ciudad, strconv.Itoa(int(in.Intvalue)))
 		count, ok := mapaDos[in.Planeta]
 		if ok == true {
 			var r1 reloj
 			r1 = mapaDos[in.Planeta]
-			if in.Servidor == 1{
+			if in.Servidor == 1 {
 				r1.x = r1.x + 1
-			}else if in.Servidor == 2{
+			} else if in.Servidor == 2 {
 				r1.y = r1.y + 1
-			}else{
+			} else {
 				r1.z = r1.z + 1
 			}
 			r1.servidor = int(in.Servidor)
 			mapaDos[in.Planeta] = r1
-		}else{
+		} else {
 			fmt.Println("el elemento no estaba", count)
 			if s.merge == true {
+				ClearRegistro(strconv.Itoa(int(in.Servidor)))
 				p = true
 				s.merge = false
-			}else{
+			} else {
 				p = false
 			}
-			return &Reloj{Planeta: "",X:int32(0),Y:int32(0),Z:int32(0),Merge:p},nil
+			return &Reloj{Planeta: "", X: int32(0), Y: int32(0), Z: int32(0), Merge: p}, nil
 		}
-	}else{
-		log.Printf("Caso contrario")	
+	} else {
+		log.Printf("Caso contrario")
 	}
+
+	if op {
+		AddRegistro(strconv.Itoa(int(in.Servidor)), in.Accion+" "+in.Planeta+" "+in.Ciudad+" "+soldados)
+	}
+
 	if s.merge == true {
+		ClearRegistro(strconv.Itoa(int(in.Servidor)))
 		p = true
 		s.merge = false
-	}else{
+	} else {
 		p = false
 	}
 	var r reloj
 	r = mapaDos[in.Planeta]
-	return &Reloj{Planeta: in.Planeta,X:int32(r.x),Y:int32(r.y),Z:int32(r.z),Merge:p},nil
-}	
+	return &Reloj{Planeta: in.Planeta, X: int32(r.x), Y: int32(r.y), Z: int32(r.z), Merge: p}, nil
+}
 
-func (s *Server) Alertabroken(ctx context.Context, in *Operacion) (*Message, error){
+func (s *Server) Alertabroken(ctx context.Context, in *Operacion) (*Message, error) {
 	n := int32(0)
 	if in.Accion != "" && in.Servidor == 0 {
 		rand.Seed(time.Now().UnixNano())
 		n = int32(1 + rand.Intn(3-1+1))
-		log.Printf("Servidor elegido es : %d",n)
-	}else{
-		n = in.Servidor	
+		log.Printf("Servidor elegido es : %d", n)
+	} else {
+		n = in.Servidor
 	}
-	return &Message{Nserver:n},nil
+	return &Message{Nserver: n}, nil
 }
 
 func (s *Server) Intermediario(ctx context.Context, in *Message) (*Message, error) {
-	log.Printf(" %d ",in.Nserver)
-	return &Message{Nserver:in.Nserver},nil
+	log.Printf(" %d ", in.Nserver)
+	return &Message{Nserver: in.Nserver}, nil
 }
 
 func (s *Server) Leia(ctx context.Context, in *L) (*Lresponse, error) {
-	log.Printf(" %s ",in.Planeta)
+	log.Printf(" %s ", in.Planeta)
 	s.planeta = in.Planeta
 	s.ciudad = in.Ciudad
-	s.servidor = in.Servidor // EVALUAR A QUE SERVER DIRIGIR 
+	s.servidor = in.Servidor // EVALUAR A QUE SERVER DIRIGIR
 	s.process = true
-	log.Printf("%t",s.process)
-	for(s.process) {
+	log.Printf("%t", s.process)
+	for s.process {
 		//log.Printf("Esta aqui for leia")
 	}
-	return &Lresponse{Planeta: s.planeta,Valor:s.Valor,X:s.X,Y:s.Y,Z:s.Z,Servidor:s.servidor}, nil
+	return &Lresponse{Planeta: s.planeta, Valor: s.Valor, X: s.X, Y: s.Y, Z: s.Z, Servidor: s.servidor}, nil
 }
 
 func (s *Server) Interleia(ctx context.Context, in *Lresponse) (*L, error) {
-	log.Printf(" %s ",in.Planeta)
+	log.Printf(" %s ", in.Planeta)
 	s.planeta = in.Planeta
 	s.Valor = in.Valor
 	s.X = in.X
@@ -419,18 +436,18 @@ func (s *Server) Interleia(ctx context.Context, in *Lresponse) (*L, error) {
 	s.servidor = in.Servidor
 	s.merge = in.Merge
 	s.process = false
-	log.Printf("%t",s.process)
-	for(!s.process){
+	log.Printf("%t", s.process)
+	for !s.process {
 		//log.Printf("Esta aqui for Interleia")
 	}
 	log.Printf("Sale del ciclo")
-	return &L{Planeta: s.planeta,Ciudad:s.ciudad,Servidor:s.servidor}, nil
+	return &L{Planeta: s.planeta, Ciudad: s.ciudad, Servidor: s.servidor}, nil
 }
 
-func (s *Server) Leiafulcrum(ctx context.Context, in *L)(*Lresponse, error){
-	log.Printf(" %s ",in.Planeta)
-	// Buscar una la respuest en el archivo 
-	return &Lresponse{Planeta: in.Planeta,Valor:0,X:0,Y:0,Z:0,Servidor:in.Servidor,Merge:s.merge}, nil
+func (s *Server) Leiafulcrum(ctx context.Context, in *L) (*Lresponse, error) {
+	log.Printf(" %s ", in.Planeta)
+	// Buscar una la respuest en el archivo
+	return &Lresponse{Planeta: in.Planeta, Valor: 0, X: 0, Y: 0, Z: 0, Servidor: in.Servidor, Merge: s.merge}, nil
 }
 
 func (s *Server) Pedirdic(ctx context.Context, in *Message) (*Merge, error) {
@@ -439,10 +456,10 @@ func (s *Server) Pedirdic(ctx context.Context, in *Message) (*Merge, error) {
 	var m = make(map[string]string)
 	for key, value := range mapaDos {
 		fmt.Println("Key:", key, "Value:", value.x)
-		m[key] = strconv.Itoa(value.x)+","+strconv.Itoa(value.y)+","+strconv.Itoa(value.z)+GetPlanet(key)
+		m[key] = strconv.Itoa(value.x) + "," + strconv.Itoa(value.y) + "," + strconv.Itoa(value.z) + GetPlanet(key)
 		// Agregar funcion josue GetPlanet(planeta string)
 	}
-	return &Merge{M:m},nil 
+	return &Merge{M: m}, nil
 }
 
 func (s *Server) Enviardic(ctx context.Context, in *Merge) (*Message, error) {
@@ -462,9 +479,9 @@ func (s *Server) Enviardic(ctx context.Context, in *Merge) (*Message, error) {
 		n, _ = strconv.Atoi(l[2])
 		r1.z = n
 		r1.servidor = 1
-		mapaDos[key]=r1
+		mapaDos[key] = r1
 		UpdatePlanet(key, l[3])
-		// Falta pasar el string a archivo 
+		// Falta pasar el string a archivo
 	}
-	return &Message{Nserver:1},nil
+	return &Message{Nserver: 1}, nil
 }
